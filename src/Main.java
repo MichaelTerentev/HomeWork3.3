f49be2391fcdf1bd8b9a767197f6aad47daf8dc9
MichaelTerentev <125468589+MichaelTerentev@users.noreply.github.com>
2023-05-03 15:43:29 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BinaryTree tree = new BinaryTree();
+        tree.put(3);
+        tree.put(11);
+        tree.put(69);
+        tree.put(121);
+        tree.remove(121);
+        tree.print();
     }
 }
